7a90ac388fb9a2b036d75b6a8317310bc27b8f77
VladimirZaitsevkm <vladimir.zaitsevvv@gmail.com>
2025-04-10 17:54:18 +0300

    extensions tranpose and trace added

---
+++ SquareMatrixExtension.cs
@@ -0,0 +1,35 @@
+﻿using lab7;
+using System;
+
+namespace lab7
+{
+  public static class SquareMatrixExtension
+  {
+    public static SquareMatrix Transpose(this SquareMatrix matrix)
+    {
+      SquareMatrix transposed = new SquareMatrix(matrix.Size);
+
+      for (int row = 0; row < matrix.Size; ++row)
+      {
+        for (int column = 0; column < matrix.Size; ++column)
+        {
+          transposed._matrix[row, column] = matrix._matrix[column, row];
+        }
+      }
+
+      return transposed;
+    }
+
+    public static int Trace(this SquareMatrix matrix)
+    {
+      int trace = 0;
+
+      for (int i = 0; i < matrix.Size; ++i)
+      {
+        trace += matrix._matrix[i, i];
+      }
+
+      return trace;
+    }
+  }
+}
